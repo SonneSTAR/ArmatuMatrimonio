227b33fd4d24a86d751c53e36094dae0d92d18f8
Ricardo <ricardo.ignacio18@hotmail.com>
2023-01-12 17:26:18 -0300

    Jquery syntax

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,16 @@
-let crearBtn = document.getElementById('crearBtn')
-let spinnerStatic = document.getElementById('spinnerStatic')
-let spinnerMove = document.getElementById('spinnerMove')
-let form = document.getElementById('form')
-crearBtn.addEventListener('click', () =>{
-    form.classList.add('fade-out')
-    spinnerStatic.classList.add('spinner');
-    spinnerStatic.classList.add('fade-in');
-    
+$(document).ready(function(){
+    let crearBtn = $('#crearBtn');
+    let spinnerStatic = $('#spinnerStatic');
+    let form = $('#form');
 
-}) 
+    crearBtn.on('click', () =>{
+        form.addClass('fade-out')
+        spinnerStatic.addClass('spinner');
+        spinnerStatic.addClass('fade-in');
+        
+
+    }) 
+    //código a ejecutar cuando el DOM está listo para recibir instrucciones.
+ });
+
+
